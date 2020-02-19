635b2a4956b085ea257d11d3465257faeccb04ed
louis1001 <luisal1207@gmail.com>
2020-02-19 17:32:22 -0600

    Retouches responsive design for services.

---
+++ assets/js/scroll_animation.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal('#landing-img', {
+    duration: 1400,
+    delay: 200
+})
+
+ScrollReveal().reveal('.service-img.anim-right', {
+    distance: '100px',
+    origin: 'left',
+    duration: 1400,
+    delay: 300,
+    viewFactor: 0.2
+})
+
+ScrollReveal().reveal('.service-img.anim-left', {
+    distance: '100px',
+    origin: 'right',
+    duration: 1400,
+    delay: 300,
+    viewFactor: 0.2
+})
